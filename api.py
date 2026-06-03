2dc4d972a6efc5d77e6386c9331dcfdc41de8cca
Shweta Gera <shweta.gera@outlook.com>
2026-06-04 01:37:51 +0530

    Testing Phase 1 (Gold Case Test for KB)_final_3

--- api.py
+++ api.py
@@ -576,9 +576,14 @@ def find_exact_kb_match(kb_id, question):
         position = text_lower.find(search_term)
 
         if search_term in text_lower:
+
             print("----- CANDIDATE -----")
             print("POSITION:", position)
-            print(text[:300])
+
+            if "definition" in text_lower:
+                print("HAS DEFINITION")
+
+            print(text[:500])
 
         if (
             any(pattern in text_lower for pattern in title_patterns)
