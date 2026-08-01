c3f800666884c5d6220738442745b65622f675a8
Shweta Gera <shweta.gera@outlook.com>
2026-08-01 16:11:19 +0530

    Refactored admin service and removed chat dependencies_1

--- api_admin.py
+++ api_admin.py
@@ -4,7 +4,6 @@ import os
 import re
 from fastapi import FastAPI, UploadFile, File, BackgroundTasks, Form
 from settings import OPENAI_MODEL
-# from google import genai
 from storage import save_file, save_metadata
 from kb_builder import read_pdf, chunk_text, create_embeddings, build_kb, save_kb
 from notifier import notify_embedding_status
@@ -18,7 +17,6 @@ from vector_db import delete_embeddings
 from prompts import build_prompt
 from typing import List, Optional
 from vector_db import query_kb_embeddings_filtered
-import google.generativeai as genai
 from db import insert_qna_sl
 from db import update_qna_sl_validation, get_qna_sl
 from db import mark_qna_ml_ready
@@ -28,10 +26,6 @@ from db import delete_qna_record
 from vector_db import delete_qna_embeddings
 from vector_db import get_all_kb_chunks
 
-
-genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
 
 app = FastAPI()
 
